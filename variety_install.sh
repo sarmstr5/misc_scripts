026865c237b9d5aa4ea9681c5d694a82c59a5008
shane armstrong <sarmstr5@git.gmail.com>
2017-10-28 11:44:55 -0400

    wallpaper changer

---
+++ variety_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#Variety is an open-source wallpaper changer for Linux
+
+sudo add-apt-repository ppa:peterlevi/ppa
+sudo apt-get update
+sudo apt-get install variety
+
+#runs variety
+variety
